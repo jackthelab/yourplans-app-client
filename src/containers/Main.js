0079f66e7ed48cc28093398edce2d68aaedd902b
Jacob Hampton <jhampton792@gmail.com>
2021-04-13 15:24:43 -0500

    Added some conditional rendering for what shows to user

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+//containers
+// import LandingPage from './LandingPage'
+// import UserLoginPage from './UserLoginPage'
+// import BusinessLoginPage from './BusinessLoginPage'
+
 const Main = () => {
 
     const accountType = useSelector(state => state.accountType)
@@ -9,6 +14,7 @@ const Main = () => {
     return (
         <div id="main-container">
 
+            {accountType === null && activityType === null ? <h1>Landing Page</h1> : null}
             {accountType === "user" && activityType === "login" ? <h1>A User Wants to Login</h1> : accountType === "user" ? <h1>A User wants to {`${activityType}`}</h1> : null }
             {accountType === "business" && activityType === "login" ? <h1>A Business Wants to Login</h1> : accountType === "business" ? <h1>A business want to {`${activityType}`}</h1> : null }
 
