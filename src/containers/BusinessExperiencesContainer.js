c049908845287b7a567eff6c882d6a9cbda3b623
Jacob Hampton <jhampton792@gmail.com>
2021-04-21 15:18:16 -0500

    Updated Business Experiences

---
+++ src/containers/BusinessExperiencesContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+// components
+import SmallExperienceCard from '../components/SmallExperienceCard'
+
+const BusinessExperiencesContainer = ({ experiences }) => {
+
+    const [futureExperiences, setFutureExperiences] = useState(experiences.filter( (exp) => !exp.complete ))
+    const [pastExperiences, setPastExperiences] = useState(experiences.filter( (exp) => exp.complete))
+
+    return (
+        <>
+            <div className="col-md-12">
+                <h2 className="section-header">Experiences</h2>
+            </div>
+            <div className="sect-col col-md-6">
+                <div>
+                    <h1>Upcoming Experiences</h1>
+                </div>
+                <div>
+                    { futureExperiences.length > 0 ? futureExperiences.map( (experience, idx) => <SmallExperienceCard experience={ experience } key={ idx } /> ) : <p>Looking for your next experience? Click the "See Open Bids" button below to get started!</p> }
+                </div>
+            </div>
+            <div className="sect-col col-md-6">
+                <div>
+                    <h1>Past Experiences</h1>
+                </div>
+                <div>
+                    { pastExperiences.length > 0 ? pastExperiences.map( (experience, idx) => <SmallExperienceCard experience={ experience } key={ idx } /> ) : <p>Looking for your next experience? Click the "See Open Bids" button below to get started!</p> }
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default BusinessExperiencesContainer
--- src/containers/BusinessProfileHome.js
+++ src/containers/BusinessProfileHome.js
@@ -3,29 +3,28 @@ import { useSelector } from 'react-redux'
 
 // containers
 // import BusinessBidResponsesContainer from './BusinessBidResponsesContainer'
-// import BusinessExperiencesContainer from './BusinessExperiencesContainer'
+import BusinessExperiencesContainer from './BusinessExperiencesContainer'
 // import BusinessStatsContainer from './BusinessStatsContainer'
 
 const BusinessProfileHome = () => {
 
     const profileDetails = useSelector(state => state.profile)
     
-    const [businessBidResponses, setBusinessBidResponses] = useState(profileDetails.bid_responses.length)
-    const [businessExperiences, setBusinessExperiences] = useState(profileDetails.experiences.length)
+    const [businessBidResponses, setBusinessBidResponses] = useState(profileDetails.bid_responses)
+    const [businessExperiences, setBusinessExperiences] = useState(profileDetails.experiences)
     
     return (
         
-        <div className="row">
+        <div>
 
-            <div className="sect-col col-md-4">
+            <div className="row">                
+                <BusinessExperiencesContainer experiences={ businessExperiences } />
+            </div>
+            <div className="row">
                 <h2>Bid Responses</h2>
                 <p>{`No. of Bid Responses: ${ businessBidResponses }`}</p>
             </div>
-            <div className="sect-col col-md-4">
-                <h2>Experiences</h2>
-                <p>{`No. of Experiences: ${ businessExperiences }`}</p>
-            </div>
-            <div className="sect-col col-md-4">
+            <div className="row">
                 <h2>Business Dashboard Stats</h2>
                 <ul>
                     <li>Deals Won?</li>
